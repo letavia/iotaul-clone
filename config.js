7645e8bb286111423120eb2922e514fc5b883a51
letavia <letavia@users.noreply.github.com>
2019-02-03 19:37:54 +0100

    change db name
    change db name

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ config.iota = {
         //username: 'root',
         //password: 'mongodb', 
         port: 27017,
-        db: 'iotaul'
+        db: 'iotagentul'
         //replicaSet: 'kubecity-rs'
     },
     types: {},
